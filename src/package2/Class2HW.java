006a9ac7d5cb4d88a6ddf94bf1ccb8aef0adeb6c
Rimsky <Rimsky@DESKTOP-BO2IQFC.lan>
2023-04-09 21:12:04 -0400

    "Homework 2 - DONE!"

---
+++ src/package2/Class2HW.java
@@ -0,0 +1,14 @@
+package package2;
+
+public class Class2HW {
+	
+	public static void main (String args []) {
+		
+		System.out.println("Practice, practice and more practice");
+	}
+	
+	public void add (int a, int b) {
+		System.out.println(a + b);
+	}
+
+}
